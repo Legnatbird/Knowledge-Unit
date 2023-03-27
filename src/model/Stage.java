1367c46971e078c94205ba40f23fae8d1d4708a3
Alejandro Quiñones Caicedo <50637445+Legnatbird@users.noreply.github.com>
2023-03-27 16:52:24 +0000

    Implement real project time This help tracking the development of the project

--- src/model/Stage.java
+++ src/model/Stage.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.util.GregorianCalendar;
+
 public class Stage {
 
+  private IntervalTime realTime;
+  private GregorianCalendar startDate;
+  private GregorianCalendar endDate;
   private IntervalTime plannedTime;
   private boolean active = false;
   private String stageName;
 
   public Stage(String name) {
     this.stageName = name;
+    this.startDate = new GregorianCalendar();
   }
 
   public String getStageName() {
@@ -28,9 +34,19 @@ public class Stage {
 
   public void setInactive() {
     this.active = false;
+    this.endDate = new GregorianCalendar();
   }
 
   public boolean isActive() {
     return this.active;
+  }
+
+  private void setRealIntervalTime(IntervalTime time) {
+    this.realTime = new IntervalTime(time.getStartDate(), time.getEndDate());
+  }
+
+  public String getRealIntervalTime() {
+    this.setRealIntervalTime(new IntervalTime(this.startDate, this.endDate));
+    return this.realTime.toString();
   }
 }
